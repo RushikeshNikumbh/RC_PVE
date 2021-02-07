0cc992ed20af7d42f8f486a6172bc09969498f47
RushikeshNikumbh <RushikeshNikumbh@github.com>
2021-02-07 15:13:30 +0530

    first commit

---
+++ redirect.js
@@ -0,0 +1,50 @@
+	/*	document.querySelector('#myBtn1')
+		.addEventListener('click',() => {
+			window.open( 
+              "Get Involved.html", "_blank");
+        });		
+        
+        document.querySelector('#myBtn')
+		.addEventListener('click',() => {
+			window.open( 
+              "rcpve.com", "_blank");
+        });	*/	
+        
+        function joinRotary() {
+            window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAZ__p4VmBZURExQVllRNFVMVTBNQlBYQkxGMTdUV1A5MC4u', "_blank")
+          }
+
+
+          function joinRotaract() {
+            window.open('https://www.rotary.org/en/get-involved/rotaract-clubs', "_blank")
+          }
+
+
+          function getStarted() {
+            window.open('Get Involved.html', "_blank")
+          }
+
+          function makeDonation(){
+                window.open('https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAZ__p4VmBZURElBVUUzNzU4R0hZMzREUktTRlNUQlBSUS4u', "_blank")
+          }
+
+          function fb(){
+                window.open('https://www.facebook.com/rcpvibranteast', "_blank")
+          }
+          function twitter(){
+            window.open('https://www.twitter.com/rcpvibranteast', "_blank")
+            }
+      function yt(){
+            window.open('https://www.youtube.com/channel/UCLrvjcao_Q7odGXqWF0NI5Q/featured', "_blank")
+      }
+      function insta(){
+            window.open('https://www.instagram.com/rcpvibranteast', "_blank")
+      }
+
+      
+      function userSignup() {
+            window.open('http://rcpve.org/pph/meetings/?id=601adewebsitercpve', "_blank")
+          }
+          function clubSignup() {
+            window.open('http://rcpve.org/pph/meetings/club-signup.php', "_blank")
+          }
